113c8b398019abdffb36e2bc36d991f588ddb1d3
Artemijs Matusev <matu6ev@gmail.com>
2024-10-06 13:12:54 +0100

    Added upload directory to the gitignore file, created a basic form for the video submission

--- js/resourcepane_video.js
+++ js/resourcepane_video.js
@@ -1,7 +1,7 @@
 function add_video_resource_body(tab_id) {
     var tab_body = $(`
         <div id="` + tab_id + `_body" class="resource_pane_tab_content">
-            <form action="/upload" method="POST" enctype="multipart/form-data id="video-form">
+            <form action="/upload-video" method="POST" enctype="multipart/form-data id="video-form">
                 <input type="file" name="video" accept="video/*" required>
                 <button type="submit"> Upload Video </button> 
             </form>
